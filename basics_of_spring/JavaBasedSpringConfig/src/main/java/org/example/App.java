33a96308900011a5f94c3c81f710392c1a1accb8
Bhavin Gupta <gutbhavi@amazon.com>
2024-04-12 20:00:18 +0530

    Java based spring application basic1

---
+++ basics_of_spring/JavaBasedSpringConfig/src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println( "Hello World!" );
+
+        Desktop desktop = context.getBean(Desktop.class);
+        desktop.switchOn();
+    }
+}
---
+++ basics_of_spring/JavaBasedSpringConfig/src/main/java/org/example/Desktop.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Desktop {
+
+    public Desktop(){
+
+    }
+
+    public void switchOn(){
+        System.out.println("desktop is on");
+    }
+}
